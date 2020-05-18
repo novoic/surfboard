2a2ac2ee65efa054738e6027e1cb467df1146b18
Raphael Lenain <raphael.lenain@gmail.com>
2020-05-18 10:49:23 +0100

    :sparkles: add librosa mocking to the conf

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,17 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
+# Mock module to bypass pip install
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = [
+    'librosa', 'librosa.display'
+]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- Project information -----------------------------------------------------
 
